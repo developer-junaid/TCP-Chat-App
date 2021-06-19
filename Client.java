c0f392012da4238a4ef01dcf93d86a71e8c2c500
developer-junaid <qjunaid623@gmail.com>
2021-06-19 20:14:42 +0500

    created chat app

--- Client.java
+++ Client.java
@@ -2,6 +2,7 @@ import java.io.*; // {Scanner}
 import java.net.*; // {Socket}
 import java.util.*; // {PrintWriter}
 
+// Junaid Qureshi
 // Client
 public class Client {
 
@@ -11,7 +12,6 @@ public class Client {
 
         Socket link = new Socket(InetAddress.getLocalHost(), 8081); // Step 1: Server connection request
 
-        System.out.println("Send message to server");
         // create streams Step 2, 3 and 4
         Scanner input = new Scanner(link.getInputStream());
         PrintWriter output = new PrintWriter(link.getOutputStream(), true);
@@ -21,7 +21,8 @@ public class Client {
         String msg = "";
 
         while (!msg.equalsIgnoreCase("close")) {
-            // Echo
+            // Chat Logic
+            System.out.print("Send message to server: ");
             msg = cmdInput.nextLine(); // Get from input user
             output.println(msg); // Send to server
             msg = input.nextLine(); // Get from server
--- Server.java
+++ Server.java
@@ -2,6 +2,7 @@ import java.io.*; // {Scanner}
 import java.net.*; // {ServerSocket, Socket}
 import java.util.*; // {PrintWriter}
 
+// Junaid Qureshi
 // Server
 public class Server {
     // Main
@@ -18,14 +19,22 @@ public class Server {
         // create streams Step 3 and 4
         Scanner input = new Scanner(link.getInputStream());
         PrintWriter output = new PrintWriter(link.getOutputStream(), true);
+        Scanner cmdInput = new Scanner(System.in); // Get input from user's screen
 
         // Step 5: Echo messages untill "close"
         String msg = "";
 
         while (!msg.equalsIgnoreCase("close")) {
-            // Echo
+            // 1 Take input from client
+            // 2 Print client's message
+            // 3 Ask server about message
+            // 4 Send that to client
+            // 5 Print server's message
+            // Ask again
             msg = input.nextLine(); // Get from client
             System.out.println("Client Says: " + msg); // Print
+            System.out.print("Send message to client: "); // Ask
+            msg = cmdInput.nextLine(); // Ask server what to send
             output.println(msg); // Send to client
         }
 
